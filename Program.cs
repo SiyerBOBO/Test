a45a01cee4e113adefbe5bd41314d551917c9942
博博 <1162472111@qq.com>
2021-03-04 11:59:34 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Threading;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using(SQLiteConnection _sql_conn = new SQLiteConnection())
+			{
+				_sql_conn.ConnectionString += "data source = test.db;";
+				_sql_conn.ConnectionString += "version = 3;";
+				try
+				{
+					_sql_conn.Open();
+					using(SQLiteCommand _sql_cmd = _sql_conn.CreateCommand())
+					{
+						_sql_cmd.CommandText = "create table if not exists \"tab\" (\"tmsp\" text);";
+						_sql_cmd.ExecuteNonQuery();
+
+						while (true)
+						{
+							Thread.Sleep(1000);
+							_sql_cmd.CommandText = "insert into tab values(@tmsp);";
+							_sql_cmd.Parameters.AddWithValue("@tmsp", DateTime.Now.ToString());
+							_sql_cmd.ExecuteNonQuery();
+							Console.WriteLine("Success!");
+						}
+					}
+				}
+				catch(Exception e)
+				{
+					Console.WriteLine(e.ToString());
+				}
+			}
+		}
+	}
+}
